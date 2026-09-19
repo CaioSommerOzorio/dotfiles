f0effced2e6d4630a406db8005ddc6340f196d82
CaioSommerOzorio <caio.sommerozorio@gmail.com>
2026-09-19 19:26:45 +1000

    monochrome theme

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    cmdline = {
+      enabled = true,
+      view = "cmdline",
+    },
+  },
+}
